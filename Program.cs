0df0afa3300e759459714777e7be51896cbc5f5d
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-06-09 12:42:17 -0400

    fixed issue with city win condition

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ int cannonRange;
 
 Console.WriteLine("Player 1, how far away from the city do you want to station the Manticore?");
 int manticorePosition = Convert.ToInt32(Console.ReadLine());
+
 Console.Clear();
+
 Console.WriteLine("Player 2, it is your turn.");
 
 while (cityHealth > 0 && manticoreHealth > 0) // condition is always true, you don't need a do while loop
@@ -17,25 +19,26 @@ while (cityHealth > 0 && manticoreHealth > 0) // condition is always true, you d
     Console.WriteLine($"The cannon is expected to deal {cannonDamage} this round.");
     Console.WriteLine("Enter desired cannon range: ");
     cannonRange = Convert.ToInt32(Console.ReadLine());
+
     ShotPlacement();
-    cityHealth--;
-    
-    if (manticoreHealth == 0)
-    {
-        Console.WriteLine("The Manticore has been destroyed! The city of Consolas is safe!");
-    }
-    else if (cityHealth == 0)
-    {
-        Console.WriteLine("The Manticore has destroyed the city of Consolas! Player 1 wins!");
-    }
-    else
-    {
-        gameRound++;
-    }
-} 
+
+    gameRound++;
+
+    if (manticoreHealth > 0) cityHealth--;
+}
+
+if (manticoreHealth <= 0)
+{
+    Console.WriteLine("The Manticore has been destroyed! The city of Consolas is safe!");
+}
+else
+{
+    Console.WriteLine("The Manticore has destroyed the city of Consolas! Player 1 wins!");
+}
 
 
-void ShotPlacement() // get a target range from the second player, and resolve it's effect. tell the user if they over shoot, undershoot or hit the manticore. If hit reduce manticore health.
+void
+    ShotPlacement() // get a target range from the second player, and resolve it's effect. tell the user if they over shoot, undershoot or hit the manticore. If hit reduce manticore health.
 {
     if (cannonRange > manticorePosition)
     {
@@ -47,27 +50,26 @@ void ShotPlacement() // get a target range from the second player, and resolve i
     }
     else
     {
-        Console.WriteLine("That round was a DIRECT HIT!"); 
+        Console.WriteLine("That round was a DIRECT HIT!");
         manticoreHealth = manticoreHealth - cannonDamage;
     }
 }
 
 void Cannon()
 {
-        if (gameRound % 5 == 0 || gameRound % 3 == 0)
-        {
-            cannonDamage = 3;
-        }
-        else if (gameRound % 5 == 0 && gameRound % 3 == 0)
-        {
-            cannonDamage = 10;
-        }
-        else
-        {
-            cannonDamage = 1;
-        }
+    if (gameRound % 5 == 0 || gameRound % 3 == 0)
+    {
+        cannonDamage = 3;
+    }
+    else if (gameRound % 5 == 0 && gameRound % 3 == 0)
+    {
+        cannonDamage = 10;
+    }
+    else
+    {
+        cannonDamage = 1;
+    }
 }
-
 
 
 Console.ReadKey();
